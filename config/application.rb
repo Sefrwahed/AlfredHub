e156b2d7d906770c2baa75258a31b8eb877206b9
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2017-02-10 15:38:41 +0200

    gdrive fix try ++

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module AlfredHub
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/lib)
+    #config.autoload_paths += %W(#{config.root}/lib)
     config.generators do |g|
       g.assets false
       g.helper false
